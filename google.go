9a768b5487722aa6f55bf942e1e0e46c25fa6f1f
andrewyang17 <50484992+andrewyang17@users.noreply.github.com>
2022-03-23 14:38:02 +0800

    refactor code and add speech adaption feature

--- google.go
+++ google.go
@@ -1,7 +1,5 @@
 // Package goEagi of google.go provides a simplified interface
 // for calling Google's speech to text service.
-// It provides flexibility to the callers and allow them to
-// set their desired configuration.
 
 package goEagi
 
@@ -9,12 +7,16 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"io"
 	"os"
 	"strings"
 
 	speech "cloud.google.com/go/speech/apiv1"
 	speechpb "google.golang.org/genproto/googleapis/cloud/speech/v1"
+)
+
+const (
+	sampleRate  = 8000
+	domainModel = "phone_call"
 )
 
 type GoogleResult struct {
@@ -23,57 +25,40 @@ type GoogleResult struct {
 }
 
 // GoogleService provides information to Google Speech Recognizer
-// as well as methods on calling speech to text.
+// and speech to text methods.
 type GoogleService struct {
 	languageCode   string
-	model          string
 	privateKeyPath string
-	sampleRate     int32
 	enhancedMode   bool
+	speechContext  []string
 	client         speechpb.Speech_StreamingRecognizeClient
 }
 
 // NewGoogleService is a constructor of GoogleService,
 // it takes a privateKeyPath to set it in environment with key GOOGLE_APPLICATION_CREDENTIALS,
-// and a languageCode, example ["en-GB", "en-US", "en-SG", ...], see [Language Support](https://cloud.google.com/speech-to-text/docs/languages).
-func NewGoogleService(privateKeyPath string, languageCode string) (*GoogleService, error) {
+// a languageCode, example ["en-GB", "en-US", "ch", ...], see (https://cloud.google.com/speech-to-text/docs/languages),
+// and a speech context, see (https://cloud.google.com/speech-to-text/docs/speech-adaptation).
+func NewGoogleService(privateKeyPath string, languageCode string, speechContext []string) (*GoogleService, error) {
 	if len(strings.TrimSpace(privateKeyPath)) == 0 {
 		return nil, errors.New("private key path is empty")
 	}
 
 	err := os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", privateKeyPath)
 	if err != nil {
-		return nil, fmt.Errorf("failed to set env: %v\n", err)
+		return nil, fmt.Errorf("failed to set Google credential's env: %v\n", err)
 	}
 
-	var g GoogleService
-	var enhancedMode bool
-
-	enhancedLanguageCode := []string{
-		"es-US", "en-GB", "en-US", "fr-FR", "ja-JP", "pt-BR", "ru-RU",
+	g := GoogleService{
+		languageCode:   languageCode,
+		privateKeyPath: privateKeyPath,
+		enhancedMode:   false,
+		speechContext:  speechContext,
 	}
 
-	for _, v := range enhancedLanguageCode {
+	for _, v := range supportedEnhancedMode() {
 		if v == languageCode {
-			enhancedMode = true
-
-			g = GoogleService{
-				languageCode:   languageCode,
-				model:          "phone_call",
-				privateKeyPath: privateKeyPath,
-				sampleRate:     8000,
-				enhancedMode:   enhancedMode,
-			}
+			g.enhancedMode = true
 			break
-		}
-	}
-
-	if !enhancedMode {
-		g = GoogleService{
-			languageCode:   languageCode,
-			privateKeyPath: privateKeyPath,
-			sampleRate:     8000,
-			enhancedMode:   enhancedMode,
 		}
 	}
 
@@ -89,15 +74,18 @@ func NewGoogleService(privateKeyPath string, languageCode string) (*GoogleServic
 		return nil, err
 	}
 
+	sc := &speechpb.SpeechContext{Phrases: speechContext}
+
 	if err := g.client.Send(&speechpb.StreamingRecognizeRequest{
 		StreamingRequest: &speechpb.StreamingRecognizeRequest_StreamingConfig{
 			StreamingConfig: &speechpb.StreamingRecognitionConfig{
 				Config: &speechpb.RecognitionConfig{
 					Encoding:        speechpb.RecognitionConfig_LINEAR16,
-					SampleRateHertz: g.sampleRate,
+					SampleRateHertz: sampleRate,
 					LanguageCode:    g.languageCode,
-					Model:           g.model,
+					Model:           domainModel,
 					UseEnhanced:     g.enhancedMode,
+					SpeechContexts:  []*speechpb.SpeechContext{sc},
 				},
 			},
 		},
@@ -108,7 +96,7 @@ func NewGoogleService(privateKeyPath string, languageCode string) (*GoogleServic
 	return &g, nil
 }
 
-// StartStreaming takes a reading channel of audio stream and send it
+// StartStreaming takes a reading channel of audio stream and sends it
 // as a gRPC request to Google service through the initialized client.
 // Caller should run it in a goroutine.
 func (g *GoogleService) StartStreaming(ctx context.Context, stream <-chan []byte) <-chan error {
@@ -152,13 +140,9 @@ func (g *GoogleService) SpeechToTextResponse(ctx context.Context) <-chan GoogleR
 
 			default:
 				resp, err := g.client.Recv()
-				if err == io.EOF {
-					googleResultStream <- GoogleResult{Error: err}
-					return
-				}
-
 				if err != nil {
 					googleResultStream <- GoogleResult{Error: fmt.Errorf("cannot stream results: %v", err)}
+					return
 				}
 
 				for _, result := range resp.Results {
@@ -169,4 +153,8 @@ func (g *GoogleService) SpeechToTextResponse(ctx context.Context) <-chan GoogleR
 	}()
 
 	return googleResultStream
+}
+
+func supportedEnhancedMode() []string {
+	return []string{"es-US", "en-GB", "en-US", "fr-FR", "ja-JP", "pt-BR", "ru-RU"}
 }
